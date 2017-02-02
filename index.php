5f34ac307246c21bf2513131c43f120fbce07370
pranshukohli <pranshukohli28@gmail.com>
2017-02-02 21:17:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ TESTING AZURE
 $connectionInfo = array("UID" => "vwserver@vwserver", "pwd" => "PKazure28", "Database" => "store", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:vwserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-$result = mysqli_query("select * from store where 1",$conn);
+$result = mysqli_query($conn, "select * from store where 1");
 
 while ($row = mysqli_fetch_array($result)){
 	echo '1';
