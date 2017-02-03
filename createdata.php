6d37627aba9f80d651c68196df38808442940f13
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 15:18:32 +0530

    Update createdata.php

--- createdata.php
+++ createdata.php
@@ -6,24 +6,4 @@
 		</HEAD>
 </HTML>
 
-<?php
-if( $_GET["q1"] || $_GET["q2"] ) {
-	echo 'start';
-$connectionInfo = array("UID" => "vwserver@vwserver", "pwd" => "PKazure28", "Database" => "store", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:vwserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-	if( $conn ) {
-     echo "Connection established.<br>";
-}else{
-     echo "Connection could not be established.<br>";
-     die( print_r( sqlsrv_errors(), true));
 
-
-
-	$result = sqlsrv_query($conn, "insert into table1 values($_GET['q1'],$_GET['q2']");
-	echo '2j';
-	}
-	echo '3';
-	sqlsrv_close( $conn);
-}
-?>
