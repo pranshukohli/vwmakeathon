e441e3c0de22ee192c4667e2ca3c5bcadbb042e1
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 17:57:10 +0530

    Update conv.php

--- conv.php
+++ conv.php
@@ -10,7 +10,7 @@
 
 		$data = file_get_contents ('https://thingspeak.com/channels/222498/field/1.json');
     $json = json_decode($data, TRUE);
-		echo $json;
+		print $json->{'channel'};
 echo "asdhajs";
     echo ('<pre> print the json ');
     print_r ($json);
