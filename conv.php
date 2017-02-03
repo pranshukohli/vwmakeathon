c0831e7e7e8e69cbf3a6f5ffb9dce6d3eed48acb
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 17:32:10 +0530

    Update conv.php

--- conv.php
+++ conv.php
@@ -1,6 +1,15 @@
+<HTML>
+	<HEAD>
+		<TITLE>
+			JSONTOARRAY
+		</TITLE>
+	</HEAD>
+	<BODY>
 <?php
 $str = file_get_contents('https://thingspeak.com/channels/222498/field/1.json');
 	$json = json_decode($str, true);
 $data = json_decode($jasonval, TRUE);
 	echo '<pre>' . print_r($json, true) . '</pre>';
   ?>
+	</BODY>
+</HTML>
