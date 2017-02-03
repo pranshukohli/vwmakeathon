75d94de180178c5f0417c8b6da4b4b602c9fa0df
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 23:31:28 +0530

    Update getlast.php

--- getlast.php
+++ getlast.php
@@ -19,13 +19,11 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
      echo "Connection could not be established.<br>";
      die( print_r( sqlsrv_errors(), true));
 }
-	$result = sqlsrv_query($conn, "SELECT * FROM table1 WHERE value_1 > 0 ORDER BY value_1 DESC LIMIT 1");
-	echo '2j';
+	$result = sqlsrv_query($conn, "SELECT * FROM table1;");
 while($row = sqlsrv_fetch_array($result)){
    
 }
 	 echo $row['col1'].", ".$row['col2']."<br>";
-	echo '3';
 	sqlsrv_close( $conn);
 ?>
 	</body>
