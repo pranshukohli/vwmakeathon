b41054d7c66af95db0bcd4ff3bade48c50c38b51
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 18:29:22 +0530

    Update conv.php

--- conv.php
+++ conv.php
@@ -16,8 +16,7 @@ $url = "http://maps.google.com/maps/api/geocode/json?address=$address";
 //call api
 $json = file_get_contents($url);
 $json = json_decode($json);
-$lat = $json->result[0]->address_components[0]->long_name;
-
+$lat = $json->results[0]->geometry->location->lat;
 echo "Latitude: " . $lat;
   ?>
 	</BODY>
