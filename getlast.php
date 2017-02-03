142bfaae9fc91bc9d43c5a3b40f348b14ae557c0
pranshukohli <pranshukohli28@gmail.com>
2017-02-03 23:25:11 +0530

    Create getlast.php

---
+++ getlast.php
@@ -0,0 +1,19 @@
+<?php
+	echo 'start';
+$connectionInfo = array("UID" => "vwserver@vwserver", "pwd" => "PKazure28", "Database" => "store", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:vwserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+	if( $conn ) {
+     echo "Connection established.<br>";
+}else{
+     echo "Connection could not be established.<br>";
+     die( print_r( sqlsrv_errors(), true));
+}
+	$result = sqlsrv_query($conn, "SELECT * FROM table1 WHERE value_1 > 0 ORDER BY value_1 DESC LIMIT 1");
+	echo '2j';
+$row = sqlsrv_fetch_array($result)
+    echo $row['col1'].", ".$row['col2']."<br>";
+
+	echo '3';
+	sqlsrv_close( $conn);
+?>
