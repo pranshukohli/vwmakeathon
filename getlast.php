935875080d2fc58732e07bf3c3237ee4488b67b3
pranshukohli <pranshukohli28@gmail.com>
2017-02-04 10:18:49 +0530

    Update getlast.php

--- getlast.php
+++ getlast.php
@@ -21,7 +21,13 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 }
 	$result = sqlsrv_query($conn, "SELECT TOP 1 * FROM table1 ORDER BY col1 DESC");
 	$row = sqlsrv_fetch_array($result);
-	echo '<h1>WELL_1_LEVEL: '.$row['col2'].'</h1>';
+	if($row['col2']<10){
+			echo '<h1>WELL_1_LEVEL:0'.$row['col2'].'</h1>';
+
+	}
+	else{
+	echo '<h1>WELL_1_LEVEL:'.$row['col2'].'</h1>';
+	}
 	echo '2';
 	sqlsrv_close( $conn);
 ?>
