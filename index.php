13cc56c356a620c6e94d2a714dcd6f1f54e6b861
pranshukohli <pranshukohli28@gmail.com>
2017-02-04 10:54:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -32,8 +32,9 @@
   
 </div>
 	<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/222498/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+<br>
+		<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="http://virtualworld.azurewebsites.net/createdata.php"></iframe>
 
-	<h1>TESTNG TESTING 123</h1>
 </body>
   
   
