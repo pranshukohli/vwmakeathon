2b89a2c9aea0fd1d24f4dd53bff11cc8daae07ff
pranshukohli <pranshukohli28@gmail.com>
2017-04-22 11:59:49 +0530

    Update createdata.php

--- createdata.php
+++ createdata.php
@@ -8,34 +8,21 @@
 		<?php
 
 	echo 'startk';
-$connectionInfo = array("UID" => "vwserver@vwserver", "pwd" => "PKazure28", "Database" => "store", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:vwserver.database.windows.net,1433";
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "pwmserver@cse322server", "pwd" => "amith@31129", "Database" => "motordata", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cse322server.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-	if( $conn ) {
-     echo "Connection established.<br>";
-}else{
-     echo "Connection could not be established.<br>";
-	}
 	
 
-$url = "https://api.thingspeak.com/channels/222498/feed/last.json?api_key=JEJ6JPJUJ389UERW";
+	$sql = "Select * from motordata";
 
-//call api
-$json = file_get_contents($url);
-$json = json_decode($json);
-$lat = $json->field1;
-echo "Latitude: " . $lat;
-
-	$sql = "INSERT INTO table1 (col2) VALUES (?)";
-	$params = array($lat);
-
-	$stmt = sqlsrv_query( $conn, $sql, $params);
+	$stmt = sqlsrv_query( $conn, $sql);
 	if( $stmt === false ) {
 		 die( print_r( sqlsrv_errors(), true));
 	}
 	else
 	{
-		echo "Record add successfully";
+		echo $stmt;
 	}
 
 
